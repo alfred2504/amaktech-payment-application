b1849f5b9d43755971b12888aa0c3eb989c09d93
alfred2504 <inforamaiv@gmail.com>
2024-10-22 13:03:15 +0200

    create starttransaction.js file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,16 +5,15 @@ const Contact = () => (
     <h2>Contact Us</h2>
     <div className="contact-info">
       <img src="/images/email-icon.png" alt="Email Icon" />
-      <p>Email: contact@business-transaction.com</p>
+      <p>Email: inforamaiv@gmail.com</p>
 
       <img src="/images/phone-icon.png" alt="Phone Icon" />
-      <p>Phone: +123 456 7890</p>
-
+      <p>Phone: +263 716 997 735</p>
       <img src="/images/whatsapp-icon.png" alt="WhatsApp Icon" />
-      <p>WhatsApp: +123 456 7890</p>
+      <p>WhatsApp: +263 716 997 735</p>
 
       <img src="/images/facebook-icon.png" alt="Facebook Icon" />
-      <p>Facebook: BusinessTransaction</p>
+      <p>Facebook: Ama Tech Solutions</p>
     </div>
   </div>
 );
