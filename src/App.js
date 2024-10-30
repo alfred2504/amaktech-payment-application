5422df694373f4c81ffb60f1f27b3e3857046028
elijah571 <elijah571>
2024-10-30 09:05:24 -0700

    header and footer made to appear on all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Signup from './components/Signup';
 import Checkout from './components/Checkout';
 import StartTransaction from './components/StartTransaction';
-import LandingPage from './components/LandingPage';  // Importing the LandingPage component
+import LandingPage from './components/LandingPage';
 import './App.css';
 import './assets/styles.css';
 
@@ -12,6 +14,7 @@ const App = () => {
   return (
     <Router>
       <div>
+        <Header />
         <Routes>
           {/* LandingPage will be the default route */}
           <Route path="/" element={<LandingPage />} />
@@ -20,6 +23,7 @@ const App = () => {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/start-transaction" element={<StartTransaction />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
