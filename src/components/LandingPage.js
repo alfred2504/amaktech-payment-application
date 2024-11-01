0812450c7695eefbf026140b0f48bb5ac2b45943
elijah571 <elijah571>
2024-11-01 01:31:29 -0700

    import of header and footer removed from landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Header from './Header';
 import HeroSection from './HeroSection';
-import Footer from './Footer';
-
 const LandingPage = () => {
   return (
     <div>
-      {/* <Header /> */}
       <HeroSection />
-      {/* <Footer /> */}
     </div>
   );
 };
